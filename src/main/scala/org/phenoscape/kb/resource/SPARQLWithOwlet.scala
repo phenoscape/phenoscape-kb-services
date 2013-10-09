68301d85eb7ecae0bc0aaa74ae0179f8d51beab2
Jim Balhoff <balhoff@nescent.org>
2013-10-09 16:10:13 -0500

    Comment.

--- src/main/scala/org/phenoscape/kb/resource/SPARQLWithOwlet.scala
+++ src/main/scala/org/phenoscape/kb/resource/SPARQLWithOwlet.scala
@@ -40,6 +40,7 @@ class SPARQLWithOwlet(@QueryParam("query") queryParam: String, @HeaderParam("Acc
       val target = client.target(App.endpoint)
       val form = new Form()
       form.param("query", expandedQuery)
+      // accept doesn't work if empty
       val response = target.request(acceptOption.getOrElse("")).post(Entity.entity(form, MediaType.APPLICATION_FORM_URLENCODED_TYPE))
       // Don't understand why we can't just pass along the response object
       //Response.fromResponse(response).entity(response.getEntity()).build()
