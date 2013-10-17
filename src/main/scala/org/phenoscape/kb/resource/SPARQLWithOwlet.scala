3cca993663bee4123bed548f8624d06297ce373a
Jim Balhoff <balhoff@nescent.org>
2013-10-17 12:44:40 -0500

    Added direct POST option to SPARQL service.

--- src/main/scala/org/phenoscape/kb/resource/SPARQLWithOwlet.scala
+++ src/main/scala/org/phenoscape/kb/resource/SPARQLWithOwlet.scala
@@ -18,6 +18,8 @@ import javax.ws.rs.client.WebTarget
 import javax.ws.rs.core.Form
 import javax.ws.rs.client.Entity
 import javax.ws.rs.core.Response.ResponseBuilder
+import javax.ws.rs.POST
+import javax.ws.rs.Consumes
 
 /**
  * This implements a SPARQL endpoint which preprocesses queries using owlet, before passing the query on
@@ -26,28 +28,46 @@ import javax.ws.rs.core.Response.ResponseBuilder
 @Path("sparql")
 class SPARQLWithOwlet(@QueryParam("query") queryParam: String, @HeaderParam("Accept") acceptParam: String) {
 
-  val queryOption: Option[String] = Option(queryParam)
-  val acceptOption: Option[String] = Option(acceptParam)
+  private val queryOption: Option[String] = Option(queryParam)
+  private val acceptOption: Option[String] = Option(acceptParam)
 
   @GET
   @Produces(Array("application/sparql-results+xml", "application/sparql-results+json", "text/tab-separated-values",
     "text/csv", "application/rdf+xml", "text/plain", "application/x-turtle", "text/rdf+n3"))
   def executeQuery(): Response = {
     if (queryOption.isDefined) {
-      val expander = new QueryExpander(App.reasoner)
-      val expandedQuery = expander.expandQueryString(queryOption.get)
-      val client = ClientBuilder.newClient()
-      val target = client.target(App.endpoint)
-      val form = new Form()
-      form.param("query", expandedQuery)
-      // accept doesn't work if empty
-      val response = target.request(acceptOption.getOrElse("")).post(Entity.entity(form, MediaType.APPLICATION_FORM_URLENCODED_TYPE))
-      // Don't understand why we can't just pass along the response object
-      //Response.fromResponse(response).entity(response.getEntity()).build()
-      Response.status(response.getStatus()).entity(response.getEntity()).build()
+      executeQuery(queryOption.get)
     } else {
       Response.status(Response.Status.BAD_REQUEST).build()
     }
+  }
+
+  @POST
+  @Consumes(Array("application/sparql-query"))
+  @Produces(Array("application/sparql-results+xml", "application/sparql-results+json", "text/tab-separated-values",
+    "text/csv", "application/rdf+xml", "text/plain", "application/x-turtle", "text/rdf+n3"))
+  def directPOSTQuery(query: String): Response = {
+    //TODO query should be interpreted as UTF-8; where is this determined?
+    if (queryOption.isDefined) {
+      Response.status(Response.Status.BAD_REQUEST).build()
+    } else {
+      executeQuery(query)
+    }
+  }
+
+  def executeQuery(query: String): Response = {
+    val expander = new QueryExpander(App.reasoner)
+    val expandedQuery = expander.expandQueryString(query)
+    val client = ClientBuilder.newClient()
+    val target = client.target(App.endpoint)
+    val form = new Form()
+    form.param("query", expandedQuery)
+    // accept doesn't work if empty
+    val response = target.request(acceptOption.getOrElse("")).post(Entity.entity(form, MediaType.APPLICATION_FORM_URLENCODED_TYPE))
+    // Don't understand why we can't just pass along the response object
+    //Response.fromResponse(response).entity(response.getEntity()).build()
+    Response.status(response.getStatus()).entity(response.getEntity()).build()
+
   }
 
 }
