c34f5ab4c306a234cca6a8086fea3b71ba0cf0f3
Jim Balhoff <balhoff@nescent.org>
2014-12-03 13:54:08 -0500

    Changed SPARQL construct queries to use non-blocking API.

--- src/main/scala/org/phenoscape/kb/App.scala
+++ src/main/scala/org/phenoscape/kb/App.scala
@@ -27,6 +27,9 @@ import system.dispatcher
 import com.hp.hpl.jena.query.QueryFactory
 import spray.can.Http
 import com.hp.hpl.jena.rdf.model.Model
+import org.apache.jena.riot.RDFDataMgr
+import com.hp.hpl.jena.rdf.model.ModelFactory
+import java.io.ByteArrayInputStream
 
 object App {
 
@@ -35,6 +38,7 @@ object App {
   val BigdataRunPriorFirst = bgp(t(Prior, RunFirst, "true" ^^ XSDDatatype.XSDboolean))
   val `application/sparql-query` = MediaTypes.register(MediaType.custom("application/sparql-query"))
   val `application/sparql-results+xml` = MediaTypes.register(MediaType.custom("application/sparql-results+xml"))
+  val `application/rdf+xml` = MediaTypes.register(MediaType.custom("application/rdf+xml"))
 
   val conf = ConfigFactory.load()
   val KBEndpoint = IRI.create(conf.getString("kb-services.kb.endpoint"))
@@ -44,22 +48,15 @@ object App {
 
   def withOwlery(triple: TripleOrPath): ElementService = service(App.OwleryEndpoint.toString, bgp(triple))
 
-  def executeSPARQLQuery(query: Query): Future[ResultSet] = sparqlQuery(Post(App.KBEndpoint.toString, query))
+  def executeSPARQLQuery(query: Query): Future[ResultSet] = sparqlSelectQuery(Post(App.KBEndpoint.toString, query))
 
   def executeSPARQLQuery[T](query: Query, resultMapper: QuerySolution => T): Future[Seq[T]] = for {
-    resultSet <- sparqlQuery(Post(App.KBEndpoint.toString, query))
+    resultSet <- sparqlSelectQuery(Post(App.KBEndpoint.toString, query))
   } yield {
     resultSet.map(resultMapper).toSeq
   }
 
-  def executeSPARQLConstructQuery(query: Query): Future[Model] = Future {
-    blocking {
-      val queryEngine = new QueryEngineHTTP(App.KBEndpoint.toString, query)
-      val model = queryEngine.execConstruct()
-      queryEngine.close()
-      model
-    }
-  }
+  def executeSPARQLConstructQuery(query: Query): Future[Model] = sparqlConstructQuery(Post(App.KBEndpoint.toString, query))
 
   def resultSetToTSV(result: ResultSet): String = {
     val outStream = new ByteArrayOutputStream()
@@ -72,12 +69,18 @@ object App {
   private implicit val SPARQLQueryMarshaller = Marshaller.delegate[Query, String](`application/sparql-query`, MediaTypes.`text/plain`)(_.toString)
   private implicit val SPARQLQueryBodyUnmarshaller = Unmarshaller.delegate[String, Query](`application/sparql-query`)(QueryFactory.create)
   private implicit val SPARQLResultsXMLUnmarshaller = Unmarshaller.delegate[String, ResultSet](`application/sparql-results+xml`)(ResultSetFactory.fromXML)
+  private implicit val RDFXMLUnmarshaller = Unmarshaller.delegate[String, Model]() { text =>
+    val model = ModelFactory.createDefaultModel
+    model.read(new ByteArrayInputStream(text.getBytes), null)
+    model
+  }
 
   def expandWithOwlet(query: Query): Future[Query] = {
     val pipeline = sendReceive ~> unmarshal[Query]
     pipeline(Post("http://pkb-new.nescent.org/owlery/kbs/phenoscape/expand", query))
   }
 
-  val sparqlQuery: HttpRequest => Future[ResultSet] = addHeader(HttpHeaders.Accept(`application/sparql-results+xml`)) ~> sendReceive ~> unmarshal[ResultSet]
+  val sparqlSelectQuery: HttpRequest => Future[ResultSet] = addHeader(HttpHeaders.Accept(`application/sparql-results+xml`)) ~> sendReceive ~> unmarshal[ResultSet]
+  val sparqlConstructQuery: HttpRequest => Future[Model] = addHeader(HttpHeaders.Accept(`application/rdf+xml`)) ~> sendReceive ~> unmarshal[Model]
 
 }
