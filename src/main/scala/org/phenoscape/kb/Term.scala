a3cdc0e1a8fff130624860668d1e4b600e1cf93e
Jim Balhoff <balhoff@nescent.org>
2015-08-04 13:01:17 -0400

    Temporary fix for incorrectly generated post-composition labels.

--- src/main/scala/org/phenoscape/kb/Term.scala
+++ src/main/scala/org/phenoscape/kb/Term.scala
@@ -65,8 +65,9 @@ object Term {
     App.executeSPARQLQuery(buildLabelsQuery(iris: _*), Term.fromMinimalQuerySolution)
   }
 
+  //FIXME remove check for named expressions once label generation has been corrected in the build
   def computedLabel(iri: IRI): Future[MinimalTerm] = (for {
-    labelOpt <- label(iri)
+    labelOpt <- if (iri.toString.startsWith(ExpressionUtil.namedExpressionPrefix) || iri.toString.startsWith(ExpressionUtil.namedSubClassPrefix)) Future.successful(None) else label(iri)
   } yield {
     labelOpt.map(Future.successful).getOrElse {
       computeLabelForAnonymousTerm(iri)
