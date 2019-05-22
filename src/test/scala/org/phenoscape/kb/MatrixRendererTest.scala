17b975f8a32e59d61be0c1a231a2b3e8c5f96922
Shalsh <shrivastava.shal@gmail.com>
2019-05-22 16:48:09 -0400

    Add test for matrix renderer

---
+++ src/test/scala/org/phenoscape/kb/MatrixRendererTest.scala
@@ -0,0 +1,13 @@
+package org.phenoscape.kb
+
+object MatrixRendererTest extends App {
+
+  val innerMap: Map[Any, Boolean] = Map("y1" -> true, "y2" -> false, "y3" -> true)
+
+  val outerMap: Map[Any, Map[Any, Boolean]] = Map("y1" -> innerMap, "y2" -> innerMap, "y3" -> innerMap)
+
+  val outputString = Similarity.matrixRendererFromMapOfMaps(outerMap)
+
+  println(outputString)
+
+}
