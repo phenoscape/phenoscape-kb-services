6e6ac6cf011d72fba783057f37de07921213cc32
Shalsh <shrivastava.shal@gmail.com>
2019-06-13 13:52:38 -0400

    Add newline in results EOF

--- src/main/scala/org/phenoscape/kb/AnatomicalEntity.scala
+++ src/main/scala/org/phenoscape/kb/AnatomicalEntity.scala
@@ -82,7 +82,7 @@ object AnatomicalEntity {
       }
       s"$row, ${values.mkString(",")}"
     }
-    s"$headers\n${matrix.mkString("\n")}"
+    s"$headers\n${matrix.mkString("\n")}\n"
   }
 
   def presenceAbsenceDependencyMatrix(iris: Set[IRI]): Future[DependencyMatrix[IRI]] = {
