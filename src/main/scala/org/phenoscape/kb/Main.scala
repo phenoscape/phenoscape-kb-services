d982aa451e4077a0deee0c005003ebf0d24f6889
Jim Balhoff <jim@balhoff.org>
2019-06-18 22:12:43 -0400

    Allow caching of POST requests. Fixes #92. (#152)

--- src/main/scala/org/phenoscape/kb/Main.scala
+++ src/main/scala/org/phenoscape/kb/Main.scala
@@ -16,11 +16,9 @@ import com.typesafe.config.ConfigFactory
 import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.HttpHeader
+import akka.http.scaladsl.model.{HttpHeader, HttpMethod, StatusCodes, Uri, headers}
 import akka.http.scaladsl.model.HttpMethods.GET
-import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.model.Uri
-import akka.http.scaladsl.model.headers
+import akka.http.scaladsl.model.HttpMethods.POST
 import akka.http.scaladsl.model.headers.ContentDispositionTypes
 import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.server.HttpApp
@@ -72,10 +70,10 @@ object Main extends HttpApp with App {
     }
   }
 
-  val cacheKeyer: PartialFunction[RequestContext, (Uri, Option[HttpHeader])] = {
-    case r: RequestContext if (r.request.method == GET) => (r.request.uri, r.request.headers.find(_.is("accept")))
+  val cacheKeyer: PartialFunction[RequestContext, (Uri, Option[HttpHeader], HttpMethod)] = {
+    case r: RequestContext if (r.request.method == GET || r.request.method == POST) => (r.request.uri, r.request.headers.find(_.is("accept")), r.request.method)
   }
-  val memoryCache = routeCache[(Uri, Option[HttpHeader])]
+  val memoryCache = routeCache[(Uri, Option[HttpHeader], HttpMethod)]
 
   val conf = ConfigFactory.load()
   val serverPort = conf.getInt("kb-services.port")
