d68c27eafba48a20e6bc9a764983fb9304ad9aa8
Shalsh <shrivastava.shal@gmail.com>
2019-09-13 11:50:54 -0400

    Migrate matrix renderer test to utest

--- src/test/scala/org/phenoscape/kb/MatrixRendererTest.scala
+++ src/test/scala/org/phenoscape/kb/MatrixRendererTest.scala
@@ -1,23 +1,26 @@
 package org.phenoscape.kb
 
-object MatrixRendererTest extends App {
-
-  val innerMap1: Map[Any, Boolean] = Map("femur" -> true, "hindlimb" -> false, "forelimb" -> false, "hand" -> false, "face" -> false, "eye" -> false) //("y3" -> true, "y2" -> false, "y1" -> true)
-  val innerMap2: Map[Any, Boolean] = Map("femur" -> true, "hindlimb" -> true, "forelimb" -> false, "hand" -> false, "face" -> false, "eye" -> false)
-  val innerMap3: Map[Any, Boolean] = Map("femur" -> false, "hindlimb" -> false, "forelimb" -> true, "hand" -> false, "face" -> false, "eye" -> false)
-  val innerMap4: Map[Any, Boolean] = Map("femur" -> false, "hindlimb" -> false, "forelimb" -> true, "hand" -> true, "face" -> false, "eye" -> false)
-  val innerMap5: Map[Any, Boolean] = Map("femur" -> false, "hindlimb" -> false, "forelimb" -> false, "hand" -> false, "face" -> true, "eye" -> true)
-  val innerMap6: Map[Any, Boolean] = Map("femur" -> false, "hindlimb" -> false, "forelimb" -> false, "hand" -> false, "face" -> false, "eye" -> true)
+import utest._
 
 
-  val outerMap: Map[Any, Map[Any, Boolean]] = Map("eye" -> innerMap6, "femur" -> innerMap1, "hindlimb" -> innerMap2, "forelimb" -> innerMap3, "hand" -> innerMap4, "face" -> innerMap5) //("y2" -> innerMap, "y3" -> innerMap, "y1" -> innerMap)
+object MatrixRendererTest extends TestSuite {
 
-  val outputString = AnatomicalEntity.matrixRendererFromMapOfMaps(DependencyMatrix(outerMap, List("femur", "hindlimb", "forelimb", "hand", "face", "eye")))
+  val tests = Tests{
 
-  println(outputString)
+    val innerMap1: Map[Any, Boolean] = Map("femur" -> true, "hindlimb" -> false, "forelimb" -> false, "hand" -> false, "face" -> false, "eye" -> false) //("y3" -> true, "y2" -> false, "y1" -> true)
+    val innerMap2: Map[Any, Boolean] = Map("femur" -> true, "hindlimb" -> true, "forelimb" -> false, "hand" -> false, "face" -> false, "eye" -> false)
+    val innerMap3: Map[Any, Boolean] = Map("femur" -> false, "hindlimb" -> false, "forelimb" -> true, "hand" -> false, "face" -> false, "eye" -> false)
+    val innerMap4: Map[Any, Boolean] = Map("femur" -> false, "hindlimb" -> false, "forelimb" -> true, "hand" -> true, "face" -> false, "eye" -> false)
+    val innerMap5: Map[Any, Boolean] = Map("femur" -> false, "hindlimb" -> false, "forelimb" -> false, "hand" -> false, "face" -> true, "eye" -> true)
+    val innerMap6: Map[Any, Boolean] = Map("femur" -> false, "hindlimb" -> false, "forelimb" -> false, "hand" -> false, "face" -> false, "eye" -> true)
 
+
+    val outerMap: Map[Any, Map[Any, Boolean]] = Map("eye" -> innerMap6, "femur" -> innerMap1, "hindlimb" -> innerMap2, "forelimb" -> innerMap3, "hand" -> innerMap4, "face" -> innerMap5) //("y2" -> innerMap, "y3" -> innerMap, "y1" -> innerMap)
+
+    'testDep - {
+      val outputString = AnatomicalEntity.matrixRendererFromMapOfMaps(DependencyMatrix(outerMap, List("femur", "hindlimb", "forelimb", "hand", "face", "eye")))
+      println(outputString)
+    }
+  }
 }
 
-//object MatrixRendererTest extends TestSuite {
-//
-//}
