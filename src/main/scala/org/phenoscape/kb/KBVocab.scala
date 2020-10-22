3c7f1a71bc4a6c82eca0cc6565d89a5877c7ada2
Shalsh <shrivastava.shal@gmail.com>
2020-10-21 22:32:15 -0400

    Bug fix; rewrite query using sparql interpolator

--- src/main/scala/org/phenoscape/kb/KBVocab.scala
+++ src/main/scala/org/phenoscape/kb/KBVocab.scala
@@ -24,6 +24,7 @@ object KBVocab {
   val hasRelatedSynonym = ObjectProperty("http://www.geneontology.org/formats/oboInOwl#hasRelatedSynonym")
   val hasNarrowSynonym = ObjectProperty("http://www.geneontology.org/formats/oboInOwl#hasNarrowSynonym")
   val hasBroadSynonym = ObjectProperty("http://www.geneontology.org/formats/oboInOwl#hasBroadSynonym")
+  val hasAnnotation = ObjectProperty("http://example.org/hasAnnotation")
   val definition = ObjectProperty("http://purl.obolibrary.org/obo/IAO_0000115")
   val owlAnnotatedSource = IRI.create(OWL2.annotatedSource.getURI)
   val owlAnnotatedProperty = IRI.create(OWL2.annotatedProperty.getURI)
